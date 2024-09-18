b72bcf75bacee04292a68b935b9e7f7875904dd5
Vaibhav Kushwaha <93552725+ProfessorAuggie@users.noreply.github.com>
2024-09-18 09:56:15 +0530

    Create 18 September.java

---
+++ September 2024/18 September.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driverclass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        //Reading total number of testcases
+        int t= sc.nextInt();
+        
+        while(t-- >0)
+        {
+            //reading the string
+            String st = sc.next();
+            
+            //calling ispar method of Paranthesis class 
+            //and printing "balanced" if it returns true
+            //else printing "not balanced"
+            if(new Solution().ispar(st) == true)
+                System.out.println("balanced");
+            else
+                System.out.println("not balanced");
+        
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution {
+    static boolean ispar(String x) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < x.length(); i++) {
+            char ch = x.charAt(i);
+            
+            if (ch == '{' || ch == '(' || ch == '[') {
+                stack.push(ch);
+            }
+            else if (ch == '}' || ch == ')' || ch == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                
+                if ((ch == '}' && top != '{') || 
+                    (ch == ')' && top != '(') || 
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
+
