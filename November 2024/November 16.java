e40562153289a90590e30786723bb0da2416629d
Vaibhav Kushwaha <93552725+ProfessorAuggie@users.noreply.github.com>
2024-11-16 23:57:47 +0530

    Create November 16.java

---
+++ November 2024/November 16.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String[] inputLine = br.readLine().split(" ");
+            int[] arr = new int[inputLine.length];
+            for (int i = 0; i < inputLine.length; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().pushZerosToEnd(arr);
+            int n = arr.length;
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        // code here
+        int j=0;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                arr[j++]=arr[i];
+            }
+        }
+        while(j<n)
+            arr[j++]=0;
+    }
+}
