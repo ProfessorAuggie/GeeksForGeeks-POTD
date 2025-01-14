1c576c4f3fc19ef4cc854c6c1a4ff11423ad3753
Vaibhav Kushwaha <93552725+ProfessorAuggie@users.noreply.github.com>
2025-01-14 11:43:01 +0530

    Create 14 January.java

---
+++ 2025/January/14 January.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(
+                    token)); // Use Integer.parseInt to parse int values
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+
+            // calling equilibriumPoint() function
+            System.out.println(obj.findEquilibrium(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the first equilibrium point in the array.
+    public static int findEquilibrium(int arr[]) {
+        int totalSum = 0;
+        for (int num : arr) {
+            totalSum += num;
+        }
+
+        int leftSum = 0; 
+        for (int i = 0; i < arr.length; i++) {
+            if (leftSum == totalSum - leftSum - arr[i]) {
+                return i; 
+            }
+
+            leftSum += arr[i];
+        }
+
+        return -1;
+    }
+}
+
