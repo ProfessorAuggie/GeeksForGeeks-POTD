be8ca353efd1b2d9ab781d761f78783923fba64d
Vaibhav Kushwaha <93552725+ProfessorAuggie@users.noreply.github.com>
2025-02-19 22:05:46 +0530

    Create 19 February.java

---
+++ 2025/February/19 February.java
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class Main {
+    /* Function to print nodes in a given linked list */
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    // Driver program to test the above functions
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = Integer.parseInt(scanner.nextLine());
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(scanner.nextLine());
+            List<Node> lists = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                String line = scanner.nextLine();
+                String[] values = line.split(" ");
+                Node head = null, temp = null;
+
+                for (String value : values) {
+                    Node newNode = new Node(Integer.parseInt(value));
+                    if (head == null) {
+                        head = newNode;
+                        temp = head;
+                    } else {
+                        temp.next = newNode;
+                        temp = temp.next;
+                    }
+                }
+
+                lists.add(head);
+            }
+
+            Solution sol = new Solution();
+            Node head = sol.mergeKLists(lists);
+
+            printList(head);
+            System.out.println("~");
+        }
+        scanner.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+/*class Node
+{
+    int data;
+    Node next;
+
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+// arr is an array of Nodes of the heads of linked lists
+
+class Solution {
+    public Node mergeKLists(List<Node> lists) {
+        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.data));
+        for (Node head : lists) if (head != null) pq.add(head);
+        Node dummy = new Node(0), tail = dummy;
+        while (!pq.isEmpty()) {
+            tail.next = pq.poll();
+            tail = tail.next;
+            if (tail.next != null) pq.add(tail.next);
+        }
+        return dummy.next;
+    }
+}
