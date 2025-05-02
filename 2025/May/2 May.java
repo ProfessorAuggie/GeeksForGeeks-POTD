ea609f23765c940c7220e869a1ee8353b693655a
Vaibhav Kushwaha <93552725+ProfessorAuggie@users.noreply.github.com>
2025-05-02 21:16:24 +0530

    Create 2 May.java

---
+++ 2025/May/2 May.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] str = br.readLine().trim().split(" ");
+            int[] arr = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            int ans = new Solution().findMaximum(arr);
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int findMaximum(int[] arr) {
+        int low = 0, high = arr.length - 1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] < arr[mid + 1])
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        return arr[low];
+    }
+}
